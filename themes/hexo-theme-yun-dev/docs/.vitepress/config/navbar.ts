69e1138a0df826f5f3f3329072b087d13765b803
mitzi <3093917656@stud.tjut.edu.cn>
2025-10-23 19:42:29 +0800

    自动部署: 2025-10-23 19:42:24

--- themes/hexo-theme-yun-dev/docs/.vitepress/config/navbar.ts
+++
@@ -1,9 +0,0 @@
-import type { DefaultTheme } from 'vitepress'
-
-export function nav(): DefaultTheme.Config['nav'] {
-  return [
-    { text: 'Guide', link: '/guide/' },
-    { text: 'Demo', link: '/demo/' },
-    { text: 'Sponsors', link: 'https://sponsors.yunyoujun.cn' },
-  ]
-}
--- themes/hexo-theme-yun-dev/packages/hexo-theme-yun/scripts/events/data.js
+++
@@ -1,9 +0,0 @@
-const fs = require('node:fs')
-const path = require('node:path')
-const process = require('node:process')
-
-module.exports = (hexo) => {
-  // set default config
-  const aboutSite = path.resolve(process.cwd(), 'source/about/site.md')
-  hexo.theme.config.hasAboutSite = fs.existsSync(aboutSite)
-}
